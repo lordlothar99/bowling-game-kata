e371029d3c318445d3423cd0cad5b782cb931ca8
François Lecomte <francois.lecomte@gmail.com>
2020-04-14 16:41:51 +0200

    refactor

--- src/test/java/com/bowling/BowlingGameTest.java
+++ src/test/java/com/bowling/BowlingGameTest.java
@@ -5,18 +5,15 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BowlingGameTest {
+    private BowlingGame bowlingGame = new BowlingGame();
 
     @Test
     public void should_score_be_0_when_game_starts() {
-        BowlingGame bowlingGame = new BowlingGame();
-
         assertThat(bowlingGame.score()).isEqualTo(0);
     }
 
     @Test
     public void should_score_equal_number_of_pins_down_when_player_hits_pins() {
-        BowlingGame bowlingGame = new BowlingGame();
-
         bowlingGame.roll(4);
 
         assertThat(bowlingGame.score()).isEqualTo(4);
@@ -24,7 +21,6 @@ public class BowlingGameTest {
 
     @Test
     public void should_score_equal_the_sum_of_pins_down_when_player_hits_pins() {
-        BowlingGame bowlingGame = new BowlingGame();
         bowlingGame.roll(4);
 
         bowlingGame.roll(4);
