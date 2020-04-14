a70be707d1487c55cc7f25668be60fbe9382bf27
François Lecomte <francois.lecomte@gmail.com>
2020-04-14 16:37:23 +0200

    should_score_equal_the_sum_of_pins_down_when_player_hits_pins : red

--- src/test/java/com/bowling/BowlingGameTest.java
+++ src/test/java/com/bowling/BowlingGameTest.java
@@ -21,4 +21,14 @@ public class BowlingGameTest {
 
         assertThat(bowlingGame.score()).isEqualTo(4);
     }
+
+    @Test
+    public void should_score_equal_the_sum_of_pins_down_when_player_hits_pins() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(4);
+
+        bowlingGame.roll(4);
+
+        assertThat(bowlingGame.score()).isEqualTo(8);
+    }
 }
