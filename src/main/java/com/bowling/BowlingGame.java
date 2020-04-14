08d7e74003de84798e5934dc2133d6791594b52b
François Lecomte <francois.lecomte@gmail.com>
2020-04-14 16:45:51 +0200

    should_raise_an_error_when_cheater_player_hits_more_than_10_pins : done

--- src/main/java/com/bowling/BowlingGame.java
+++ src/main/java/com/bowling/BowlingGame.java
@@ -5,6 +5,9 @@ public class BowlingGame {
     private int pinsDown;
 
     public void roll(int pins) {
+        if (pins > 10) {
+            throw new RuntimeException("There are only 10 pins, cheater !");
+        }
         pinsDown += pins;
     }
 
