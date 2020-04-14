44ae9cd2a4de914fbbfcf9baeeeddc128190a9df
François Lecomte <francois.lecomte@gmail.com>
2020-04-14 16:29:55 +0200

    should_score_be_0_when_game_starts : done

--- src/test/java/com/bowling/BowlingGameTest.java
+++ src/test/java/com/bowling/BowlingGameTest.java
@@ -1,5 +1,15 @@
 package com.bowling;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BowlingGameTest {
 
+    @Test
+    public void should_score_be_0_when_game_starts() {
+        BowlingGame bowlingGame = new BowlingGame();
+
+        assertThat(bowlingGame.score()).isEqualTo(0);
+    }
 }
