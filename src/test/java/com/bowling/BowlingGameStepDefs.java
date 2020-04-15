ea22a1ae828bf1bb25f86a9f88406135c26874cb
François Lecomte <francois.lecomte@gmail.com>
2020-04-15 18:00:54 +0200

    Le score est à 0 au début de la partie : ok

--- src/test/java/com/bowling/BowlingGameStepDefs.java
+++ src/test/java/com/bowling/BowlingGameStepDefs.java
@@ -1,5 +1,22 @@
 package com.bowling;
 
+import io.cucumber.java.fr.Alors;
+import io.cucumber.java.fr.Etantdonné;
+import org.assertj.core.api.Assertions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BowlingGameStepDefs {
 
+    private BowlingGame bowlingGame;
+
+    @Etantdonné("une nouvelle partie de bowling")
+    public void une_nouvelle_partie_de_bowling() {
+        bowlingGame = new BowlingGame();
+    }
+
+    @Alors("le score est égal à {int}")
+    public void le_score_est_égal_à(int score) {
+        assertThat(bowlingGame.score()).isEqualTo(score);
+    }
 }
