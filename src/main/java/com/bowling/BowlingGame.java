91e9af90a109f08cabfd6836806109f64644f45a
François Lecomte <francois.lecomte@gmail.com>
2020-04-15 18:15:14 +0200

    Le score après deux lancers est égal à la somme des quilles tombées : ok

--- src/main/java/com/bowling/BowlingGame.java
+++ src/main/java/com/bowling/BowlingGame.java
@@ -5,7 +5,7 @@ public class BowlingGame {
 	private int quillesTombees;
 
 	public void lancer(int quillesTombees) {
-		this.quillesTombees = quillesTombees;
+		this.quillesTombees += quillesTombees;
 	}
 
 	public int score() {
