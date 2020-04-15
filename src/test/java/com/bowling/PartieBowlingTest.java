4eea4b658d2839ac252630adf082ba09d9d64daf
François Lecomte <francois.lecomte@gmail.com>
2020-04-15 20:26:17 +0200

    html report

--- src/test/java/com/bowling/PartieBowlingTest.java
+++ src/test/java/com/bowling/PartieBowlingTest.java
@@ -1,9 +1,13 @@
 package com.bowling;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = { "pretty", "html:target/cucumber-reports"  }
+)
 public class PartieBowlingTest {
 
 }
